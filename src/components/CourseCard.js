577ae41bb9536c5ac5cfc91f0a68afb00b8861cb
Bsatayu <satayu_s@cmu.ac.th>
2021-01-30 14:29:32 +0700

    commit

--- src/components/CourseCard.js
+++ src/components/CourseCard.js
@@ -1,4 +1,10 @@
 export const CourseCard = (props) => {
-  // TODO: design HTML component that displays a course as a "card" on the webpage.
+  return <>
+  <p>{props.name}</p>
+  <p>{props.grd}</p>
+  <p>{props.crd}</p>
+  <button onClick = {() =>props.del(props.name)} > x </button>
+  </>;
+};
   return (null);
 };
